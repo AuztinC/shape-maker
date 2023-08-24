ea2a4edbf99602b0461442a40b12b69975c13c22
Austin Cripe <aaustinc@live.com>
2023-08-24 13:56:11 -0400

    guided prac change.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const container = document.querySelector('.container');
-const colors = ['red', 'blue', 'green'];
-const sizes = ['sm', 'md', 'lg'];
+const colors = ['red', 'blue', 'green', "yellow"];
+const sizes = ['sm', 'md', 'lg', 'xl'];
+const shapeArr = ['circle', 'square'];
 
 function randomShape(){
   const colorIdx = Math.floor(Math.random()*colors.length);
   const sizeIdx = Math.floor(Math.random()*sizes.length);
+  const shapeIdx = Math.floor(Math.random()*shapeArr.length);
   const shape = {
     color: colors[colorIdx],
-    size: sizes[sizeIdx]
-
+    size: sizes[sizeIdx],
+    shape: shapeArr[shapeIdx]
   };
   return shape;
 }
@@ -19,15 +21,18 @@ const shapes = [
 ];
 
 function render(){
-  const html = shapes.map(function(shape){
-    return `<div class='${shape.color} ${shape.size}'></div>`;
+  const html = shapes.map(function(shape, idx){
+    return `<div class='${shape.color} ${shape.size} ${shape.shape}'> ${idx + 1} </div>`;
   });
   container.innerHTML = html.join('');
 }
 
 render();
-setInterval(function(){
+let interval = setInterval(function(){
   const newShape = randomShape();
   shapes.push(newShape);
   render();
+  if(shapes.length + 1 > 5){
+    clearInterval(interval)
+  }
 }, 500);
